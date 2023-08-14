9c6f67938934f280863db8e8d78b37504adbf1c3
Spyro-Mancer <willianhenriquevb@gmail.com>
2023-08-14 11:53:47 -0300

    Atualização Banco de Dados

---
+++ PadariaCarmel/Conectar.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace PadariaCarmel
+{
+    class Conectar
+    {
+        private static string connString = "Server=localhost;Database=dbpadariacarmel;Uid=senac;Pwd=Locaweb@102030";
+        private static MySqlConnection conn = null;
+
+        public static MySqlConnection obterConexao()
+        {
+            conn = new MySqlConnection(connString);
+            try
+            {
+                conn.Open();
+            }
+            catch (MySqlException)
+            {
+                conn = null;
+            }
+            return conn;
+        }
+        public static void fecharConexao()
+        {
+            if (conn != null)
+            {
+                conn.Close();
+            }
+        }
+    }
+}
